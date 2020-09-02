0684a9da0caafef3b3b1b7345e3d67758a8cc946
Opilane TTHK <opilane@tthk.ee>
2020-09-02 15:45:53 +0300

    Add project files.

---
+++ IljaH2/IljaH2/StackPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IljaH2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StackPage : ContentPage
+    {
+        public StackPage()
+        {
+            InitializeComponent();
+        }
+        int clik = 0;
+        private void btn_Clicked(object sender, EventArgs e)
+        {
+            clik++;
+            btn.Text = clik.ToString();
+
+            if (clik % 5 == 0)
+            {
+                lbl.Text = " ";
+            }
+            else
+            {
+                //lbl.IsVisible = true;
+                lbl.Text = "Nupp!";
+            }
+
+        }
+        private void btn2_Clicked_1(object sender, EventArgs e)
+        {
+            clik = 0;
+        }
+    }
+}
